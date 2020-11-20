4a03c9316bebb17066122e2532eee32d7ebf8191
ariel_grubbs <arielvictor1234@gmail.com>
2020-11-20 14:30:04 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(nameString, emailString) {
+    return fetch( 'http://localhost:3000/users',
+    {method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name: `${nameString}`,
+        email: `${emailString}`
+      })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object[ "id" ]
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+}
+
+submitData()
